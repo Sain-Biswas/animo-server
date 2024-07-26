344283e4b7cc92ba455dbaa764d67915ecfaf251
Sain-Biswas <sain.biswas614@gmail.com>
2024-07-26 11:16:50 +0530

    Add Route for Top-Airing Recently-Updated Most-Popular Most-Favourite Latest-Completed Recently-Added

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv"
 import zoro from "./client/Zoro";
 import home from "./routes/home";
+import AnimePageType from "./types/animepage.type";
 
 
 dotenv.config();
@@ -12,9 +13,158 @@ const port = process.env.PORT || 3000;
 
 app.use("/home", home);
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Animo - Server API");
+});
+
 app.get('/top-airing/:page', async (req: Request, res: Response) => {
   const response = await zoro.fetchTopAiring(Number.parseInt(req.params.page));
-  res.json(response)
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
+});
+
+app.get('/recently-updated/:page', async (req: Request, res: Response) => {
+  const response = await zoro.fetchRecentlyUpdated(Number.parseInt(req.params.page));
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
+});
+
+app.get('/most-favourite/:page', async (req: Request, res: Response) => {
+  const response = await zoro.fetchMostFavorite(Number.parseInt(req.params.page));
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
+});
+
+app.get('/most-popular/:page', async (req: Request, res: Response) => {
+  const response = await zoro.fetchMostPopular(Number.parseInt(req.params.page));
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
+});
+
+app.get('/latest-completed/:page', async (req: Request, res: Response) => {
+  const response = await zoro.fetchLatestCompleted(Number.parseInt(req.params.page));
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
+});
+
+app.get('/recently-added/:page', async (req: Request, res: Response) => {
+  const response = await zoro.fetchRecentlyAdded(Number.parseInt(req.params.page));
+
+  const data : AnimePageType = {
+    currentPage: response.currentPage,
+    hasNextPage: response.hasNextPage,
+    totalPages: response.totalPages,
+    results: []
+  }
+
+  data.results = response.results.map((anime) => {
+    return {
+      id: anime.id,
+      title: anime.title,
+      titleJapanese: anime.japaneseTitle,
+      banner: anime.image,
+      duration: anime.duration,
+      episodes: anime.sub,
+      type: anime.type
+  }
+  })
+
+  res.json(data);
 });
 
 app.listen(port, () => {
--- api/types/animepage.type.ts
+++ api/types/animepage.type.ts
@@ -2,8 +2,8 @@ import AnimeCardType from "./animecard.type";
 
 
 export default interface AnimePageType {
-    currentPage: number;
-    hasNextPage: boolean;
-    totalPages: number;
+    currentPage: number | undefined;
+    hasNextPage: boolean | undefined;
+    totalPages: number | undefined;
     results: AnimeCardType[]
 }
